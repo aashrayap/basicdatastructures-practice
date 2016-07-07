329a8d9217173e86a84491f642cdae506579314f
aashraya <aashraya_p@hotmail.com>
2016-07-07 17:01:15 -0500

    first commit

---
+++ StackandQueue/queue.rb
@@ -0,0 +1,11 @@
+class Stack
+  def initialize
+  	@array=[0,1,2,3,4]
+  end
+  def pushh(anything)
+  	length=@array.length
+  	temp=Array.new(length+1)
+  	i=0
+  	while i<length
+  		temp[i]=@array
+  end
