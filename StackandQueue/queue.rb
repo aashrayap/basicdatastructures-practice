e15422524e467bb470e4b54e87d4aa7f89571bdf
aashraya <aashraya_p@hotmail.com>
2016-07-07 18:51:03 -0500

    did queue

---
+++ StackandQueue/queue.rb
@@ -0,0 +1,32 @@
+class Queue
+  def initialize
+    @array=[]
+    @backindex=0
+    @frontindex=0
+  end
+  attr_accessor :array, :backindex
+
+  def enqueue(whatever)
+    @array[@backindex]=whatever
+    @endindex+=1
+    @array
+  end
+
+  def dequeue
+    @array[frontindex]=nil
+    @frontindex+=1
+    @array
+  end
+
+  def display
+  i=0
+    while i<@array.length
+      if !@array[i].nil?
+        print @array[i]
+      end
+      i+=1
+    end
+  end
+
+
+end
