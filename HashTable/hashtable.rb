45320d8a68941bcbbb30f9e5b911a25105182445
aashraya <aashraya_p@hotmail.com>
2016-07-08 13:38:30 -0500

    finished hash method

--- HashTable/hashtable.rb
+++ HashTable/hashtable.rb
@@ -1,5 +1,26 @@
 class HashTable
+  def intitialize
+  	@buckets=Array.new
+  end
 
+  def hash(word)
+  	first_letter=word.downcase[0]
+  	checker='a'
+  	array_index=0
+  	until checker=="aa"
+  		return array_index if checker==first_letter
+  		array_index+=1
+  		checker.next!
+  	end
+  	puts "error message"
+  end
+
+  # def insert(word)
+  # 	index=hash(word)
 
 
 end
+
+h=HashTable.new
+a=h.hash('hi')
+puts a
